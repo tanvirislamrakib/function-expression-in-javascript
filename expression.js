95bbafaf247711c11d889cdb9b1c2c18f28aae8c
tanvirislamrakib <tanvirislamrakib93@gmail.com>
2024-01-10 10:12:50 +0600

    expression in js

---
+++ expression.js
@@ -0,0 +1,20 @@
+var ExpressionFun = function CallME(){
+    console.log("HAblu Programmer")
+}
+ExpressionFun();
+
+var func = function(){
+    console.log("Rakib");
+}
+func();
+//  hablu programmer er javascript class 33
+
+var name = function(name1, name2){
+    console.log(name1, name2);
+}
+name("I love javascript", "I love python");
+
+var pogramming = function(no1, no2){
+    console.log(no1, no2);
+}
+pogramming("javascript", "python");
